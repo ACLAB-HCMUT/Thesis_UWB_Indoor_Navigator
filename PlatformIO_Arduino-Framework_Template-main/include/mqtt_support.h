6b31b5bb87170ce4fe7038ea2a84e4791d4ee319
ngynmhieu <ngyn.mhieu@gmail.com>
2024-09-30 12:12:10 +0700

    update 3

--- PlatformIO_Arduino-Framework_Template-main/include/mqtt_support.h
+++ PlatformIO_Arduino-Framework_Template-main/include/mqtt_support.h
@@ -5,10 +5,11 @@
 
 #define AIO_SERVER "io.adafruit.com"
 #define AIO_SERVERPORT 1883
-#define AIO_USERNAME "your_username"
-#define AIO_KEY "your_key"
+#define AIO_USERNAME "aclab241"
+#define AIO_KEY "aio_vgIF36pDKlW42rjNwdkYCgzuuYIw"
 
 void mqttTask(void *pvParameters);
+void publishCoordinate(void *pvParameters);
 extern WiFiClient client;
 extern Adafruit_MQTT_Client mqtt;
 
