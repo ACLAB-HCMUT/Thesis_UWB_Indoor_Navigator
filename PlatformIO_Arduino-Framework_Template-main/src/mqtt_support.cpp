84c06f6d4b04c0440957e2850a1fdb200b1a31af
ngynmhieu <ngyn.mhieu@gmail.com>
2024-09-30 12:14:40 +0700

    update 4

--- PlatformIO_Arduino-Framework_Template-main/src/mqtt_support.cpp
+++ PlatformIO_Arduino-Framework_Template-main/src/mqtt_support.cpp
@@ -5,6 +5,7 @@ Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO
 Adafruit_MQTT_Publish coordinate = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/coordinate");
 
 void mqttTask(void *pvParameters){
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
     if (mqtt.connect()){
         Serial.println("MQTT Connected");
     } else {
@@ -26,9 +27,17 @@ float randomFloat (float minValue, float maxValue){
     return minValue + random * (maxValue - minValue);
 }
 
-void publishCoordinate(){
-    float coordinateValue = randomFloat(0, 90);
-    if (mqtt.connected()){
-        coordinate.publish(coordinateValue);
+void publishCoordinate(void *pvParameters){
+    vTaskDelay(8000 / portTICK_PERIOD_MS);
+    int count = 0;
+    while (count < 99){
+        float coordinateValue = randomFloat(0, 90);
+        if (mqtt.connected()){
+            coordinate.publish(coordinateValue);
+            Serial.print("Publish coordinate value: ");
+            Serial.print(coordinateValue);
+            Serial.println();
+        }
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
     }
 }
