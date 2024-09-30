a23736c23a4ccf04ef817ff55c1db964deecb3ab
ngynmhieu <ngyn.mhieu@gmail.com>
2024-09-30 17:55:12 +0700

    update 4

--- PlatformIO_Arduino-Framework_Template-main/src/main.cpp
+++ PlatformIO_Arduino-Framework_Template-main/src/main.cpp
@@ -2,11 +2,9 @@
 
 void setup(){
   Serial.begin(115200);
-  // pinMode(ledPin, OUTPUT);
 
   xTaskCreate(wifiTask, "WiFiTask", 4096, NULL, 1, NULL);
-  // xTaskCreate(mqttTask, "MQTTTask", 4096, NULL, 1, NULL);
-  // xTaskCreate(serverTask, "ServerTask", 8192, NULL, 1, NULL);
+  xTaskCreate(mqttTask, "MQTTTask", 4096, NULL, 1, NULL);
 }
 
 void loop(){
