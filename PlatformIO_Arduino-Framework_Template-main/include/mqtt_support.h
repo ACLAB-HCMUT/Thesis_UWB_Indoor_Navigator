8aef52dc0d960f56b5b2f0c4eab77dda23ad6055
ngynmhieu <ngyn.mhieu@gmail.com>
2024-10-07 11:41:08 +0700

    config adafruit info

--- PlatformIO_Arduino-Framework_Template-main/include/mqtt_support.h
+++ PlatformIO_Arduino-Framework_Template-main/include/mqtt_support.h
@@ -3,11 +3,6 @@
 
 #include "global.h"
 
-#define AIO_SERVER "io.adafruit.com"
-#define AIO_SERVERPORT 1883
-#define AIO_USERNAME "your_username"
-#define AIO_KEY "your_key"
-
 void mqttTask(void *pvParameters);
 void publishCoordinate(void *pvParameters);
 extern WiFiClient client;
--- PlatformIO_Arduino-Framework_Template-main/project_config.h
+++ PlatformIO_Arduino-Framework_Template-main/project_config.h
@@ -1,2 +1,7 @@
 #define PROJECT_WIFI_SSID "RD-SEAI_2.4G"
 #define PROJECT_WIFI_PASSWORD "ACLAB2024"
+
+#define AIO_SERVER "io.adafruit.com"
+#define AIO_SERVERPORT 1883
+#define AIO_USERNAME "your_username"
+#define AIO_KEY "your_password"
