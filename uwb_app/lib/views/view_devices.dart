d84415c82a2f2e7792536de235f4654cad6d11d2
ngynmhieu <ngyn.mhieu@gmail.com>
2024-10-12 13:05:08 +0700

    insert needed libraries and design cards

--- uwb_app/lib/views/view_devices.dart
+++ uwb_app/lib/views/view_devices.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
+class Device {
+  final String deviceName;
+  final String deviceAcStatus;
+  final String deviceLoStatus;
+  final String deviceImagePath;
+
+  Device ({
+    required this.deviceName,
+    required this.deviceAcStatus,
+    required this.deviceLoStatus,
+    required this.deviceImagePath,
+  });
+}
+
+List<Device> devices = [
+  Device(
+    deviceName: 'Node 1',
+    deviceAcStatus: 'Active',
+    deviceLoStatus: 'In room',
+    deviceImagePath: 'assets/images/node1.png',
+  ),
+  Device(
+    deviceName: 'Node 2',
+    deviceAcStatus: 'Not Active',
+    deviceLoStatus: 'In room',
+    deviceImagePath: 'assets/images/node2.png',
+  ),
+  Device(
+    deviceName: 'Node 3',
+    deviceAcStatus: 'Active',
+    deviceLoStatus: 'Out room',
+    deviceImagePath: 'assets/images/node3.png',
+  ),
+];
+
 class ViewDevices extends StatefulWidget {
   const ViewDevices({super.key});
 
@@ -10,9 +45,40 @@ class ViewDevices extends StatefulWidget {
 class _ViewDevicesState extends State<ViewDevices> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Devices'),
+    return MaterialApp(
+      title: 'Devices',
+      home: Scaffold(
+        body: GridView.builder(
+          itemCount: devices.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            childAspectRatio: 3/4,
+          ),
+          itemBuilder: (context, index) {
+            final device = devices[index];
+            return Card (
+              elevation: 4.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: const Image(
+                      image: AssetImage('uwb.png'),
+                      height: 120.0,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ],)
+            );
+          },
+        )
       ),
     );
   }
