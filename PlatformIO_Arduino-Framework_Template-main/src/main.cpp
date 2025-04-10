86a06996c8d406f8e1be3d3862af84ab0007a1d0
Huy11042003 <coipham1104@gmail.com>
2025-04-11 01:22:43 +0700

    update

--- PlatformIO_Arduino-Framework_Template-main/src/main.cpp
+++ PlatformIO_Arduino-Framework_Template-main/src/main.cpp
@@ -8,92 +8,21 @@
 
 // Create a NeoPixel object
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-
+int mqttConnectedSignal = 0;
 String DATA  = " ";  // Used to store distance data
 int UWB_MODE = 0;    // Used to set UWB mode
 
 int UWB_T_UI_NUMBER_2 = 0;  // flag bit
 int UWB_T_UI_NUMBER_1 = 0;
-int UWB_T_NUMBER      = 1;
+int UWB_T_NUMBER      = 0;
 int UWB_B_NUMBER      = 0;
+int DELAY             = 0;
 
 hw_timer_t *timer   = NULL;
 int timer_flag      = 0;
 int base_flag       = 0;
 uint32_t timer_data = 0;
 static void IRAM_ATTR Timer0_CallBack(void);
-
-// Data display via Serial
-void UWB_display() {
-    // if (Serial2.available()) {
-    //     Serial.println(UWB_MODE);
-    // }
-    switch (UWB_MODE) {
-        case 0:  // Tag mode
-            if (UWB_T_NUMBER > 0 && UWB_T_NUMBER < 5) {
-                int c = UWB_T_NUMBER;
-                // int b = 4 - UWB_T_NUMBER;
-                // while (c > 0) {
-                //     c--;
-                    Serial.print("Tag serial number: ");
-                    Serial2.write("AT+version?\r\n");
-                    DATA = Serial2.readString();
-                    Serial.println(DATA);  // Tag serial number
-                    Serial.print("Distance: ");
-                    DATA = Serial2.readString();
-                    Serial.println(DATA);  // Distance
-                // }
-                // while (b > 0) {
-                //     b--;
-                //     Serial.println("Clearing remaining data slots...");
-                // }
-            }
-            break;
-        case 1:  // Base station mode
-            if (UWB_B_NUMBER == 1) {
-                Serial.println("Base station data: ");
-                Serial.println(DATA);  // Display data in Base station mode
-            }
-            break;
-    }
-}
-
-// UI display via Serial
-void UWB_ui_display() {
-    Serial.println("UWB Example");
-    Serial.println("Tag: press BtnA");
-    Serial.println("Base: press BtnB");
-    Serial.println("Reset: press BtnC");
-
-    switch (UWB_MODE) {
-        case 0:  // Tag mode UI display
-            if (UWB_T_NUMBER > 0 && UWB_T_NUMBER < 5) {
-                int c = UWB_T_NUMBER;
-                int b = 4 - UWB_T_NUMBER;
-                while (c > 0) {
-                    c--;
-                    Serial.print("Tag ");
-                    Serial.print(c + 1);  // Tag number
-                    Serial.print(" - Distance: ");
-                    Serial.println("M");  // Example distance
-                }
-                while (b > 0) {
-                    b--;
-                    Serial.println("Clearing additional data slots...");
-                }
-            }
-            break;
-        case 1:  // Base station mode UI display
-            Serial.print("Base station ID: ");
-            Serial.println(UWB_B_NUMBER);
-            if (UWB_B_NUMBER == 0) {
-                Serial.println("Loading...");
-            } else {
-                Serial.println("Data loaded successfully.");
-            }
-            break;
-    }
-}
 
 // Display and data clear
 void UWB_clear() {
@@ -109,71 +38,19 @@ void UWB_clear() {
     Serial.println("Data cleared.");
 }
 
-// Read UART data
-void UWB_readString() {
-    switch (UWB_MODE) {
-        case 0:  // Tag mode
-            if (Serial2.available()) {
-                delay(20);
-                UWB_T_NUMBER = (Serial2.available() / 11);  // Count the number of Base stations
-                delay(20);
-                if (UWB_T_NUMBER != UWB_T_UI_NUMBER_1 || UWB_T_UI_NUMBER_2 == 0) {  
-                    UWB_T_UI_NUMBER_1 = UWB_T_NUMBER;
-                    UWB_T_UI_NUMBER_2 = 1;
-
-                    // Display tag mode info on Serial
-                    Serial.print("Tag Mode: Number of base stations: ");
-                    Serial.println(UWB_T_NUMBER);
-                }
-                DATA = Serial2.readString();
-                delay(2);
-                timer_flag = 0;
-                timer_data = 1;
-                break;
-            } else {
-                timer_flag = 1;
-            }
-            if (timer_data == 0 || timer_data > 8) {  // Check connection with base station
-                DATA       = "  0 2F   ";
-                timer_flag = 0;
-
-                // No base station connected, send message to serial
-                Serial.println("No base station connected.");
-            }
-            break;
-        case 1:  // Base station mode
-            if (timer_data == 0 || timer_data > 70) {  // Indicates successful or lost connection with Tag
-                if (Serial2.available()) {
-                    delay(2);
-                    DATA       = Serial2.readString();
-                    DATA       = "set up successfully!";
-                    timer_data = 1;
-                    timer_flag = 1;
-                    Serial.println(DATA);  // Output connection success to serial
-                    break;
-                } else if (timer_data > 0 && Serial2.available() == 0) {
-                    DATA       = "Can't find the tag!!!";
-                    timer_flag = 0;
-                    Serial.println(DATA);  // Output error message to serial
-                    break;
-                }
-            }
-            break;
-    }
-}
-
 // AT command setup
 void UWB_setupmode() {
     switch (UWB_MODE) {
         case 0:
             for (int b = 0; b < 2; b++) {  // Repeat twice to stabilize the connection
                 delay(50);
-                Serial2.write("AT+anchor_tag=0\r\n");  // Set up the Tag
+                Serial2.write("AT+anchor_tag=0,");  // Set up the Tag
+                Serial2.print(UWB_T_NUMBER);  // Output base station ID to serial
+                Serial2.write(",");  // Set up the Tag
+                Serial2.print(DELAY);  // Output base station ID to serial
+                Serial2.write("\r\n");
                 Serial.println("Set up Tag");
                 delay(50);
-                Serial2.write("AT+interval=5\r\n");  // Set the calculation precision
-                delay(50);
-                Serial2.write("AT+switchdis=1\r\n");  // Start measuring distance
                 Serial.println("Start measuring");
                 delay(50);
                 if (b == 0) {
@@ -202,32 +79,7 @@ void UWB_setupmode() {
     }
 }
 
-// Handle button presses
-void UWB_Keyscan() {
-    if (M5.BtnA.isPressed()) {
-        UWB_MODE = 0;
-        UWB_setupmode();
-        UWB_clear();
-        UWB_T_UI_NUMBER_2 = 0;
-        Serial.println("Switched to Tag mode.");
-    }
-    if (M5.BtnB.isPressed()) {
-        UWB_MODE = 1;
-        if (UWB_B_NUMBER == 4) {
-            UWB_B_NUMBER = 0;
-        }
-        UWB_setupmode();
-        UWB_clear();
-        UWB_B_NUMBER++;
-        Serial.println("Switched to Base station mode.");
-    }
-    if (M5.BtnC.isPressed()) {
-        Serial2.write("AT+RST\r\n");
-        UWB_setupmode();
-        UWB_clear();
-        Serial.println("System reset.");
-    }
-}
+
 
 // Timer initialization
 void UWB_Timer() {
@@ -263,17 +115,19 @@ void setup() {
     delay(100);
     UWB_setupmode();
     UWB_Timer();
-    // UWB_ui_display();
-
+    Serial2.write("AT+RST\r\n");
     xTaskCreate(wifiTask, "WiFiTask", 4096, NULL, 1, NULL);
-    // xTaskCreate(mqttTask, "MQTTTask", 4096, NULL, 1, NULL);
-    // xTaskCreate(publishCoordinate, "publishCoordinate", 4096, NULL, 1, NULL);
+    xTaskCreate(mqttTask, "MQTTTask", 4096, NULL, 1, NULL);
 }
 
 void loop() {
     M5.update();
-    // UWB_Keyscan();
-    // UWB_readString();
-    UWB_display();
-    // Serial.println(UWB_MODE);  // Print UWB mode to Serial2 for debugging
+
+    DATA = Serial2.readString();
+    Serial.println(DATA);
+    delay(50);
+    
+    if(DATA.indexOf("B0") != -1){
+        publishCoordinate(DATA);
+    }
 }
